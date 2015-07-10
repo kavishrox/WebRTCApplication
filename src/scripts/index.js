130b197d69959748023b7a1278aadedb2562da2e
Kavish Dwivedi <dwivedikavish@gmail.com>
2015-07-10 12:29:36 +0530

    [Kavish - Local media get and node js server]

---
+++ src/scripts/index.js
@@ -0,0 +1,18 @@
+var server = require("./server");
+var log = require("./log").log;
+var port = process.argv[2] || 5001;
+
+//returns 404
+function fourohfour(info) {
+    var res = info.res;
+    log("Request handler fourohfour was called.");
+    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.write("404 Page Not Found");
+    res.end();
+}
+
+var handle = {};
+handle["/"] = fourohfour;
+
+server.serveFilePath("");
+server.start(handle, port);
